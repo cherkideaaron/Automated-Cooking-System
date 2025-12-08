a2b5cb0d1a448c8479e421b1db2114f9a319c3b6
kevindeaaron53 <133815435+kevindeaaron53@users.noreply.github.com>
2025-12-08 15:39:35 +0300

    Project Initialization

---
+++ frontend/app/screens/HomeScreen.tsx
@@ -0,0 +1,26 @@
+// /app/screens/HomeScreen.tsx
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Home Screen</Text>
+      <Text>Here you can see your recipes and robot status.</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 8,
+  },
+});
